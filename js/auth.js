e6a0830a16fe056518cd37cd062113261ff8c310
Gustavo Arantes <gustavolucio27@gmail.com>
2018-10-23 11:15:57 -0300

    Funções de login e log-out criadas

---
+++ js/auth.js
@@ -0,0 +1,22 @@
+// Login function
+function Login(){
+    //Getting email and password value
+    var email = document.getElementById("email_value").value;
+    var password = document.getElementById("pass_value").value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+}
+
+// Logout function
+function Logout(){
+    firebase.auth().signOut().then(function() {
+        // Sign-out successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
+}
