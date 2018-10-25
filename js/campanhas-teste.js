9ea538a07dd3a92b7f9814f603b5d65388a97ef9
Gustavo Arantes <gustavolucio27@gmail.com>
2018-10-25 12:14:52 -0300

    Corrigido erro de portugues

--- js/campanhas-teste.js
+++ js/campanhas-teste.js
@@ -13,9 +13,9 @@ db.collection("campanhas").get().then(function(querySnapshot) {
         var tituloc = doc.data().titulo;
         var desc = doc.data().descricao;
         var patrocinio_img = doc.data().imagem_patrocinio;
-        var link_patrocino = doc.data().link_patrocinio;
+        var link_patrocinio = doc.data().link_patrocinio;
 
-        areaCampanha.innerHTML += "<div class='box-campanha'><img class='imagem' src='" + img + "'><div class='progresso'><div class='barra'><div class='concluido'>" + porcentagem + "</div></div><span class='arrecadado'>" + arrecadado + "</span><br><span>arrecadado de </span><span class='valor-total'>" + total + "</span></div><div class='informacoes'><div class='titulo'>" + tituloc + "</div><div class='descricao'>" + desc + "</div><div class='area-patrocinio'><span class='patrocinio'>patrocinado por</span><a href='" + link_patrocinio + "' class='clicavel'><img class='img-patrocinio' src='" + patrocinio_img + "'></a></div></div></div>";
-        areaCampanha.innerHTML = "<p> hello world </p>";
+        //areaCampanha.innerHTML += "<div class='box-campanha'><img class='imagem' src='" + img + "'><div class='progresso'><div class='barra'><div class='concluido'>" + porcentagem + "</div></div><span class='arrecadado'>" + arrecadado + "</span><br><span>arrecadado de </span><span class='valor-total'>" + total + "</span></div><div class='informacoes'><div class='titulo'>" + tituloc + "</div><div class='descricao'>" + desc + "</div><div class='area-patrocinio'><span class='patrocinio'>patrocinado por</span><a href='" + link_patrocinio + "' class='clicavel'><img class='img-patrocinio' src='" + patrocinio_img + "'></a></div></div></div>";
+        //areaCampanha.innerHTML = "<p> hello world </p>";
     });
 });
